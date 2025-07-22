5e943d6fc01cf6d9e4661cbd59badcfb8d286e1a
yarl villanueva <yarlvillamon@gmail.com>
2025-07-23 01:40:33 +0800

    Changed cv

--- public/js/main.js
+++ public/js/main.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', function() {
         downloadCVBtn.addEventListener('click', function() {
             // Create a temporary link element
             const link = document.createElement('a');
-            link.href = '/assets/ylviverse_cv.pdf';
+            link.href = '/assets/ylvvvv_CV.pdf';
             link.download = 'Yarl_Villanueva_CV.pdf';
             link.target = '_blank';
             
